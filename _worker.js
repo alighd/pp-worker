aa521010475caddee347beb9c61314c0f6a276e6
aligh_d <Ali.ghafouri.d@gmail.com>
2023-08-09 00:07:43 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -935,7 +935,9 @@ function createVLESSSub(userID_Path, hostName) {
 		}
 		// Iterate over all ports for https
 		portArray_https.forEach((port) => {
-			const commonUrlPart_https = `:${port}?encryption=none&security=tls&sni=${hostName}&type=ws&host=${hostName}&path=%2F%3Fed%3D2048#${hostName}-HTTPS`;
+			//const commonUrlPart_https = `:${port}?encryption=none&security=tls&sni=${hostName}&type=ws&host=${hostName}&path=%2F%3Fed%3D2048#${hostName}-HTTPS`;
+			const commonUrlPart_https = `:${port}?encryption=none&security=tls&sni=${hostName}&type=ws&host=${hostName}&path=%2F%3Fed%3D2048#${hostName}-HTTPS%26alpn%3Dh2%2Chttp%2F1.1%26fp%3Dchrome&alpn=h2%2Chttp%2F1.1&fp=random`;
+			
 			const vlessMainHttps = `vless://${userID}@${hostName}${commonUrlPart_https}`;
 
 			// For each proxy IP, generate a VLESS configuration and add to output
